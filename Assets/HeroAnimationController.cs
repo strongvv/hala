d5703a72fe9d10ada2231dbedd51d514830e42ef
Molochkov <v.molochkov@socialquantum.com>
2017-10-16 13:45:53 +0300

    initial

---
+++ Assets/HeroAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAnimationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	int velocity = 20;
+	void Update () {
+		if ( Input.GetKey(KeyCode.LeftArrow) ) {
+			velocity -= 10;
+		}
+		if ( Input.GetKey(KeyCode.RightArrow) ) {
+			velocity += 10;
+		}
+        transform.Rotate(0, Time.deltaTime * velocity, 0);
+    }
+}
